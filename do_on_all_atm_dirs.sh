808bb99dc4a68557e38573739e43daf610c2b1d3
Eirik Enger <een023@login-1.fram.sigma2.no>
2023-08-28 09:34:05 +0200

    fix: use correct path and script name, and improve file save name

--- do_on_all_atm_dirs.sh
+++ do_on_all_atm_dirs.sh
@@ -22,14 +22,9 @@ set -o nounset # Treat any unset variables as an error
 
 # Load modules
 module --quiet purge # Reset the modules to the system default
-# module load Python/3.8.2-GCCcore-9.3.0;
-# module load matplotlib/3.2.1-intel-2020a-Python-3.8.2;
-# module load GEOS/3.8.1-iccifort-2020.1.217-Python-3.8.2;
-# module load PROJ/7.0.0-GCCcore-9.3.0;
-# module load FFmpeg/4.2.2-GCCcore-9.3.0;
-# . /cluster/home/een023/.virtualenvs/p3/bin/activate
-module restore cesm-helper-scripts
+module load NCO/5.0.3-intel-2021b
 
+HERE="$PWD"
 # Go to where we want to be (this script could be run from anywhere)
 cd /cluster/projects/nn9817k/cesm/archive/ || exit 1
 # Select all:
@@ -37,14 +32,14 @@ DIRS=$(ls -d ./*/atm/hist)
 # Or define a custom set of dirs (but this is more easily done inside the loop, in the
 # "case" statement):
 # DIRS="one\ntwo"
-# VAR="TREFHT FLNT FSNT AODVISstdn ICEFRAC SST"
-VAR="U"
+VAR="-a FLNT -a FSNT"
 FREQ="h0"
-# Relevant for h0:
-#   TREFHT SST AODVISstdn FLNT FSNT TMSO2 ICEFRAC so4_a1 so4_a2 so4_a3
-# Relevant for h1:
-#   TREFHT AODVISstdn FLNT FSNT
+# Relevant variables for h0:
+#   -a TREFHT -a SST -a AODVISstdn -a FLNT -a FSNT -a TMSO2 -a ICEFRAC -a so4_a1 -a so4_a2 -a so4_a3 -a U
+# Relevant variables for h1:
+#   -a TREFHT -a AODVISstdn -a FLNT -a FSNT
 
+OUTPUT="-$FREQ-$(date '+%Y%m%d')"
 # Loop over all simulation directories.
 for p in $DIRS; do
     tmpdir="$p"
@@ -61,7 +56,7 @@ for p in $DIRS; do
         # We don't want to work on the `e_BASELINE` simulation. It's too long.
         continue
         ;;
-    *BWma1850-control*)
+    *EWma1850-ens1-control*)
         # Let us say we only want to include some simulation we just completed, for
         # example all simulations run with the fSST1850 "compset":
         ;;
@@ -71,15 +66,16 @@ for p in $DIRS; do
     esac
 
     # Step in, do work, then step out.
-    # The gen_agg script should be able to take a bunch of files, combine them and
-    # create a single `.nc` file for one or more of the variables. It should be saved as
-    # `<VAR_NAME>YYYYMMDD.nc`
+    # The gen_agg_nco.sh script should be able to take a bunch of files, combine them
+    # and create a single `.nc` file for one or more of the variables. It should be
+    # saved as `<VAR_NAME>YYYYMMDD.nc`. Since the frequency is also very relevant, we
+    # save it as well, and instead specify the date ourselves (above).
     cd "$p" || exit 1
     echo "Moving into $PWD"
-    echo gen_agg_auto -a "$VAR" -i \""$tmpdir"*cam."$FREQ"*\"  # -o "month"
+    echo "$HERE"/gen_agg_nco.sh "$VAR" -i \""$tmpdir"*cam."$FREQ"*\" -o "$OUTPUT"
     # We need to allow word splitting of $VAR so that they are treated as two separate
     # attributes in the command.
-    gen_agg_auto -a $VAR -i "$tmpdir*cam.$FREQ*"  # -o month
+    "$HERE"/gen_agg_nco.sh $VAR -i "$tmpdir*cam.$FREQ*" -o "$OUTPUT"
     cd - 1>/dev/null || exit 1 && echo "Going back to $PWD"
     echo ""
 done
